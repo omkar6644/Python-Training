bce55a98c40dbc49f3d830f08d9e2885cadd2617
omkar6644 <71831292+omkar6644@users.noreply.github.com>
2021-05-17 10:29:02 +0530

    Create stack.py

--- stack.py
+++ stack.py
@@ -1,6 +1,7 @@
 class Stack:
     def __init__(self):
         self.s=[]
+ 
 #add the elements to stack
     def addElement(self,data):
         self.s=self.s+[data]
@@ -14,14 +15,15 @@ class Stack:
             return ele
 #Display the elements of stack
     def display(self):
-        return self.s
+        print(self.s)
 s1=Stack()
 s1.addElement(1)
 s1.addElement(2)
 s1.addElement(3)
-print("Stack after inserting elements",s1.display())
+print("Stack after inserting elements")
+s1.display()
 s1.pop()
 print("Stack after poping:")
-print(s1.display())
+s1.display()
 
 
