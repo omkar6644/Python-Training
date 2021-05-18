6b09c7dc1022cb4f965d968b9c2b7deac22f7e5c
omkar6644 <71831292+omkar6644@users.noreply.github.com>
2021-05-18 09:00:19 +0530

    Add files via upload

---
+++ Doubly_Linked_List.py
@@ -0,0 +1,78 @@
+class Node:
+    #Node class constructor to initialize data, next, prev
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+ 
+
+class DoublyLinkedList:
+    #Linkedlist class constructor to initialize head
+    def __init__(self):
+        self.head = None
+    #adding element at beginning of list
+    def addAtBegin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+    
+    #deleting node from list
+    def deleteNode(self, x):
+        if self.head is None:
+            return
+        if self.head == x:
+            self.head = x.next
+    
+    #adding element at end of list
+    def addAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.next is not None:
+                n = n.next
+            n.next = new_node
+            new_node.prev = n
+
+    #counting the number of elements in the list
+    def count(self):
+        temp=self.head
+        count=0
+        while temp is not None:
+            count=count+1
+            temp=temp.next
+        return count
+     #printing elements of the list
+    def printList(self):
+        if self.head is None:
+            return
+        else:
+            n=self.head
+            while n is not None:
+                print(n.data)
+                n=n.next
+        
+ 
+d = DoublyLinkedList()
+print("adding at beginning")
+d.addAtBegin(1)
+d.addAtBegin(2)
+d.addAtBegin(3)
+d.printList()
+print()
+print("after deleting")
+d.deleteNode(d.head)
+d.printList()
+print()
+print()
+print("adding at end")
+d.addAtEnd(1000)
+d.printList()
+print()
+print()
+print("No of elements in list is: ",d.count())
